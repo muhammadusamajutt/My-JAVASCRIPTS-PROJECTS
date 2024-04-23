3f2b26cece5f2eead75114975ed0f02ccb7323a6
Muhammad Usama Amir <muhammadusamajut@gmail.com>
2024-04-23 11:41:55 +0500

    changes tabs

--- TABS/js/script.js
+++ TABS/js/script.js
@@ -1,17 +1,31 @@
-let shows = ' <div class="technology"><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/php.svg" alt=""> <p>PHP</p> </div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/.net.svg" alt=""> <p>.Net</p></div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/laravel.svg" alt=""><p>Laravel</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/nodejs.svg" alt=""> <p>Node.Js</p> </div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/angular.svg" alt="">  <p>Angular</p> </div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/react.svg" alt="">   <p>React.Js</p> </div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/python.svg" alt=""> <p>Python</p> </div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/html5.svg" alt=""><p>HTML 5</p> </div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/vue.svg" alt=""> <p>VueJS</p> </div></div> '
-document.getElementById('show').innerHTML = shows
+
 function webTech() {
-    document.getElementById('show').innerHTML = shows
-}
-function dbTech() {
-    document.getElementById("show").innerHTML = ' <div class="technology"><div class="tech"><img src="https://www.techgropse.com/common/images/updated/mysql.svg" alt=""><p>MySQL</p></div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/postgresql.svg" alt=""><p>PostgreSQL</p></div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/mongodb.png" alt=""><p>Mongo DB</p> </div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/cassandra.svg" alt=""><p>Cassandra</p> </div> </div>'
+    document.getElementById("tabs1").classList.remove("d-none")
+    document.getElementById("tabs2").classList.remove("d-block")
+    document.getElementById("tabs3").classList.remove("d-block")
+    document.getElementById("tabs2").classList.add("d-none")
+    document.getElementById("tabs3").classList.add("d-none")
+    // document.getElementById("tabs1").classList.add("d-block")
 }
 function mobTech() {
-    document.getElementById("show").innerHTML = '  <div class="technology"><div class="tech"><img src="https://www.techgropse.com/common/images/updated/objective-c.svg" alt=""><p>Objective-C</p></div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/swift.svg" alt=""><p>Swift</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/flutter.svg" alt=""><p>Flutter</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/react.svg" alt=""><p>React</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/kotlin.svg" alt=""> <p>Kotlin</p></div></div>'
+    document.getElementById("tabs2").classList.remove("d-none")
+    document.getElementById("tabs1").classList.remove("d-block")
+    document.getElementById("tabs3").classList.remove("d-block")
+    document.getElementById("tabs1").classList.add("d-none")
+    document.getElementById("tabs3").classList.add("d-none")
+    document.getElementById("tabs2").classList.add("d-block")
 }
 function uiUxTech() {
-    document.getElementById('show').innerHTML = '   <div class="technology">  <div class="tech">  <img src="https://www.techgropse.com/common/images/updated/photoshop.png" alt=""> <p>Photoshop</p></div> <div class="tech"> <img src="https://www.techgropse.com/common/images/updated/illustrator.png" alt=""><p>Illustrator</p>  </div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/sketch.svg" alt=""> <p>Sketch</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/figma.svg" alt=""><p>Figma</p></div><div class="tech"> <img src="https://www.techgropse.com/common/images/updated/xd.png" alt=""><p>XD</p></div></div>'
+    document.getElementById("tabs3").classList.remove("d-none")
+    document.getElementById("tabs2").classList.remove("d-block")
+    document.getElementById("tabs3").classList.remove("d-block")
+    document.getElementById("tabs2").classList.add("d-none")
+    document.getElementById("tabs1").classList.add("d-none")
+    // document.getElementById("tabs3").classList.add("d-block")
 }
-function cloudTech() {
-    document.getElementById("show").innerHTML ='     <div class="technology"><div class="tech"><img src="https://www.techgropse.com/common/images/updated/aws.svg" alt=""><p>Amazon AWS</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/azure.svg" alt=""><p>Microsoft Azure</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/googlecloud.svg" alt=""> <p>Google Cloud</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/digitalocean.svg" alt=""><p>Digital Ocean</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/firebase.svg" alt=""><p>Firebase</p> </div></div>'
-}
+// function dbTech() {
+//     document.getElementById("show").innerHTML = ' <div class="technology"><div class="tech"><img src="https://www.techgropse.com/common/images/updated/mysql.svg" alt=""><p>MySQL</p></div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/postgresql.svg" alt=""><p>PostgreSQL</p></div> <div class="tech"><img src="https://www.techgropse.com/common/images/updated/mongodb.png" alt=""><p>Mongo DB</p> </div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/cassandra.svg" alt=""><p>Cassandra</p> </div> </div>'
+// }
+// function cloudTech() {
+//     document.getElementById("show").innerHTML ='     <div class="technology"><div class="tech"><img src="https://www.techgropse.com/common/images/updated/aws.svg" alt=""><p>Amazon AWS</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/azure.svg" alt=""><p>Microsoft Azure</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/googlecloud.svg" alt=""> <p>Google Cloud</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/digitalocean.svg" alt=""><p>Digital Ocean</p></div><div class="tech"><img src="https://www.techgropse.com/common/images/updated/firebase.svg" alt=""><p>Firebase</p> </div></div>'
+// }
